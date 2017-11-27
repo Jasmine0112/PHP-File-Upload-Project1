805c97bd9619bd657c6e77f73f1178e5e04a68fb
Jasmine Morrison <Jasminemorrison12@yahoo.com>
2017-11-27 09:50:24 -0500

    Update index.php

--- classes/index.php
+++ classes/index.php
@@ -6,11 +6,11 @@ error_reporting(E_ALL);
 class Manage {
     static public  function autoload($class) 
     {
-        //you can put any file name or directory here
+        
         include './classes/'. $class . '.php';
     }
 }
 spl_autoload_register(array('Manage', 'autoload'));
-//instantiate the program object
+
 $obj = new main();
  ?>
