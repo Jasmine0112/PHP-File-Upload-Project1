427fb815024124b1a4ae80e2465aa6b341ff182d
Jasmine Morrison <Jasminemorrison12@yahoo.com>
2017-11-27 09:53:27 -0500

    Update upload.php

--- classes/upload.php
+++ classes/upload.php
@@ -1,66 +1,66 @@
 <?php 
 class upload extends page
 {
-	public function get()
-	{
-		$form = '<form action="index.php?page=upload" method="post" enctype = "multipart/form-data">';
+    public function get()
+    {
+        $form = '<form action="index.php?page=upload" method="post" enctype = "multipart/form-data">';
         $form .= '<label for = "file">filename:</label>';
         $form .= '<input type="file" name="file" id="file" >';
         $form .= '<br>';
         $form .= '<input type="submit" name="submit" id="file"><br>';
         $form .= '</form> ';
         $this->html .= $form;
-	}
+    }
 public function post() 
     {
         $target_dir = "upload/";
-        $target_file = $target_dir.$_FILES["file"]["name"];//make sure relative path of file
-		$temp = explode(".",$_FILES["file"]["name"]); 
-		$extension = end($temp);
-		if(empty($_FILES['file']['error']))
-		{
-			if($extension != 'csv')
-			{
-				page::error('the format of file must be csv');
-			}
- 	 		if (file_exists($target_file))   //Checking to confirm file is available
-        	{
-          		echo $_FILES["file"]["name"] . " the file has existed. ";
-        	}
-       		
-       		 else
-		   	{
- 				move_uploaded_file($_FILES["file"]["tmp_name"], $target_file );		
- 				//upload file to the upload/filename.csv
- 				header('Location:index.php?page=display&filename='. $_FILES["file"]["name"]);
- 				
-			}
-		}
-		else
-		{
-			switch($_FILES["file"]["error"])
-			{
-				case 1:
-					$this->html .= stringfunction::printThis("The uploaded file exceeds the upload_max_filesize directive in php.ini");
-					break;
-				case 2:
-					$this->html .= stringfunction::printThis("The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.");
-					break;
-				case 3:
-					 $this->html .= stringfunction::printThis("The uploaded file was only partially uploaded");
-					break;
-				case 4:
-					$this->html .= stringfunction::printThis("No file was uploaded");
-					break;
-				case 6:
-					$this->html .= stringfunction::printThis("Missing a temporary folder. Introduced in PHP 5.0.3.");
-					break;
-				case 7:
-					$this->html .= stringfunction::printThis("Failed to write file to disk. Introduced in PHP 5.1.0.");
-					break;
-			}
-			//echo "There are some mistake :".$_FILES["file"]["error"]."<br>";
-		}
-	}
+        $target_file = $target_dir.$_FILES["file"]["name"];
+        $temp = explode(".",$_FILES["file"]["name"]); 
+        $extension = end($temp);
+        if(empty($_FILES['file']['error']))
+        {
+            if($extension != 'csv')
+            {
+                page::error('the format of file must be csv');
+            }
+            if (file_exists($target_file))   
+            {
+                echo $_FILES["file"]["name"] . " the file has existed. ";
+            }
+            
+             else
+            {
+                move_uploaded_file($_FILES["file"]["tmp_name"], $target_file );     
+                //upload file to the upload
+                header('Location:index.php?page=display&filename='. $_FILES["file"]["name"]);
+                //Display page
+            }
+        }
+        else
+        {
+            switch($_FILES["file"]["error"])//error dispaly 
+            {
+                case 1:
+                    $this->html .= stringfunction::printThis("The uploaded file exceeds the upload_max_filesize directive in php.ini");
+                    break;
+                case 2:
+                    $this->html .= stringfunction::printThis("The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.");
+                    break;
+                case 3:
+                     $this->html .= stringfunction::printThis("The uploaded file was only partially uploaded");
+                    break;
+                case 4:
+                    $this->html .= stringfunction::printThis("No file was uploaded");
+                    break;
+                case 6:
+                    $this->html .= stringfunction::printThis("Missing a temporary folder. Introduced in PHP 5.0.3.");
+                    break;
+                case 7:
+                    $this->html .= stringfunction::printThis("Failed to write file to disk. Introduced in PHP 5.1.0.");
+                    break;
+            }
+            
+        }
+    }
 }
  ?>
